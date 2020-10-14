c25e35e5967e3b93959a2831228e4841fb4f4b39
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-14 20:17:19 +0000

    more files added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,35 @@ class ApplicationController < ActionController::Base
   render({:template=> "calculation_templates/square_results.html.erb"})
  end
 
- def calculate_square_root
-  @num = params.fetch("Number").to_f
-  @square_of_num = @num*@num
-
-  render({:template=> "calculation_templates/square_results.html.erb"})
+ def blank_square_root_form
+  render({ :template => "calculation_templates/square_root_form.html.erb"})
  end
 
+ def calculate_square_root
+  @num = params.fetch("Number").to_f
+  @square_of_num = @num**0.5
+  render({:template=> "calculation_templates/square_root_results.html.erb"})
+ end
 
+  def blank_payment_form
+  render({ :template => "calculation_templates/payment_form.html.erb"})
+ end
+
+ def calculate_payment
+  #@num = params.fetch("Number").to_f
+  #@square_of_num = @num**0.5
+
+  render({:template=> "calculation_templates/payment_results.html.erb"})
+ end
+
+   def blank_random_form
+  render({ :template => "calculation_templates/random_form.html.erb"})
+ end
+
+ def calculate_random
+  #@num = params.fetch("Number").to_f
+  #@square_of_num = @num**0.5
+
+  render({:template=> "calculation_templates/random_results.html.erb"})
+end
 end
