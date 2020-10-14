dd4205e63069857d1246cf408ec2d6fe8f08ef4f
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-14 19:56:29 +0000

    commit 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
  def blank_square_form
   render({ :template => "calculation_templates/square_form.html.erb"})
-  
-
-
  end
+
+ def calculate_square
+  @num = params.fetch("Number").to_i
+  @square_of_num = @num*@num
+
+  render({:template=> "calculation_templates/square_results.html.erb"})
+ end
+
+
+
 end
